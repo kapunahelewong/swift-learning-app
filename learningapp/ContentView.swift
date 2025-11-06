471033843a3dcd87ea2eb6e7c4d56cdc19620ec5
Kapunahele Wong <kapunahelewong@gmail.com>
2025-11-05 16:40:33 -0800

    Initial Commit

---
+++ learningapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  learningapp
+//
+//  Created by Kapunahele Wong on 11/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ learningapp/learningappApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningappApp.swift
+//  learningapp
+//
+//  Created by Kapunahele Wong on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct learningappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
